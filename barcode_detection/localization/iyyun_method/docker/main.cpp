3e9cf426eaf873420964dd7caf30a194f8aa6713
ksenmel <ksemelashenko@gmail.com>
2023-11-30 16:18:18 +0300

    code refactor in iyyun

--- barcode_detection/localization/iyyun_method/docker/main.cpp
+++ barcode_detection/localization/iyyun_method/docker/main.cpp
@@ -1,13 +1,15 @@
 #include <string>
 #include <stdlib.h>
 #include <iostream>
+#include <cstdio>
+#include <fstream>
 
 #include "gallo/gallo.h"
 #include "soros/soros.h"
 #include "yun/yun.h"
 
 // key
-const char* keys = 
+const char* keys =
     "{h        help |                      | print help message                            }"
     "{file          | /file/dir/file_name  | test image file(.bmp .jpg .png)               }";
 
@@ -20,16 +22,16 @@ int main(int argc, char* argv[])
 		cmd.printErrors();
 		return 0;
     }
-	
+
 	std::string fn = cmd.get<std::string>("file");
-	
+
 	iy::Gallo mGallo;
 	iy::Soros mSoros;
 	iy::Yun mYun;
 
 	cv::Mat frame_gray;
 	cv::Mat frame = cv::imread(fn.c_str());
-	
+
 	if(frame.data == NULL) {
 		std::cerr << "error! read image" << std::endl;
 		return -1;
@@ -43,23 +45,37 @@ int main(int argc, char* argv[])
 	cv::Rect s_rt = mSoros.process(frame_gray, 20);
 	cv::rectangle(frame, s_rt, cvScalar(255,0,0), 2);
 
-
 	std::vector<iy::YunCandidate> list_barcode = mYun.process(frame_gray);
+
+	std::ofstream output_file("/workspace/boxes/boundings.txt");
+
 	if (!list_barcode.empty())
 	{
+
+		if (!output_file.is_open()) {
+  			std::cerr << "Failed to open file" << std::endl;
+  			return -1;
+		}
+
 		for (std::vector<iy::YunCandidate>::iterator it = list_barcode.begin(); it < list_barcode.end(); it++)
 		{
 			if (it->isBarcode)
 			{
 				cv::Rect y_rt = it->roi;
 				cv::rectangle(frame, y_rt, cvScalar(0, 255, 255), 2);
+
+				output_file << y_rt.x << ", "
+                 << y_rt.y << ", "
+                 << y_rt.width << ", "
+                 << y_rt.height << std::endl;
+
 			}
 		}
 
 		list_barcode.clear();
 	}
 
-	cv::imwrite("/workspace/output/img.jpg", frame);
+	output_file.close();
 
 	return 0;
 }
--- barcode_detection/localization/iyyun_method/localize_iyyun.py
+++ barcode_detection/localization/iyyun_method/localize_iyyun.py
@@ -11,33 +11,41 @@ from pathlib import Path
 class LocalizeIyyun(Localizer):
     def get_boundings(self, input_img: np.ndarray) -> list[BoundingBox]:
         with tempfile.TemporaryDirectory() as tmp_dir:
-            file_path = Path(tmp_dir) / f"img.png"
+
+            path_to_img = Path(tmp_dir) / "img"
+            path_to_boxes = Path(tmp_dir) / "boxes"
+
+            path_to_img.mkdir()
+            path_to_boxes.mkdir()
+
+            file_path = path_to_img / f"img.png"
+
             cv2.imwrite(str(file_path), input_img)
 
             client = docker.from_env()
             container = client.containers.run(
                 "iyyun_docker",
-                volumes={tmp_dir: {"bind": "/workspace/tmp_for_img", "mode": "rw"}},
+                volumes={path_to_img: {"bind": "/workspace/img"},
+                         path_to_boxes: {"bind": "/workspace/boxes"}},
                 detach=True,
             )
             container.wait()
-
-            output = container.logs()
-
             container.remove()
 
-        lines = output.splitlines()
+            with open(path_to_boxes / "boundings.txt", "r") as file:
+                contents = file.read()
+                lines = contents.splitlines()
 
-        values = []
-        for line in lines:
-            values.append(line.decode("utf-8").split(","))
+                values = []
+                for line in lines:
+                    values.append(line.split(","))
 
-        bounding_boxes = []
-        for value in values:
-            # iyyun method returns x, y, width, height coordinates
-            bounding_box = BoundingBox(
-                int(value[0]), int(value[1]), int(value[2]), int(value[3])
-            )
-            bounding_boxes.append(bounding_box)
+                bounding_boxes = []
+                for value in values:
+                    # iyyun method returns x, y, width, height coordinates
+                    bounding_box = BoundingBox(
+                        int(value[0]), int(value[1]), int(value[2]), int(value[3])
+                    )
+                    bounding_boxes.append(bounding_box)
 
-        return bounding_boxes
+                return bounding_boxes
