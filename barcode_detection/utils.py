167f99270d48000e4ed824cab711a8db29ef12af
ksenmel <ksemelashenko@gmail.com>
2024-09-18 16:10:01 +0300

    rewrite fun `crop_helper` to new architecture

--- barcode_detection/utils.py
+++ barcode_detection/utils.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import numpy as np
 
@@ -10,5 +12,25 @@ def read_img(input_dir: str):
 
 
 def crop_img(input_img: np.ndarray, rect: BoundingBox):
-    cropped_image = input_img[rect.y1 : rect.y1 + rect.y2, rect.x1 : rect.x1 + rect.x2]
+    cropped_image = input_img[rect.y1: rect.y1 + rect.y2, rect.x1: rect.x1 + rect.x2]
     return cropped_image
+
+
+# for testing
+def crop_helper(save_dir: str, input_dir: np.ndarray, bounding_boxes_path: str):
+    with open(bounding_boxes_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            coordinates = [int(num) for num in line.split(',')]
+            print(coordinates)
+
+            if len(coordinates) == 4:
+                bbox = BoundingBox(*coordinates)  #распаковываем список в аргументы
+            else:
+                print(f"Found an error in line: {line}. Four integers expected, instead got {len(coordinates)}.")
+
+            cropped = crop_img(input_dir, bbox)
+
+            filename = f"{line}.jpg"
+            filepath = os.path.join(save_dir, filename)
+            cv2.imwrite(filepath, cropped)
