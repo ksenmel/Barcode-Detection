5fdf3eca69c6367d5500fd717e1fcf88fb0b32b2
ksenmel <ksemelashenko@gmail.com>
2024-09-18 16:24:16 +0300

    change `Localizer` and `LocalizeYolo` classes

--- barcode_detection/localization/localize.py
+++ barcode_detection/localization/localize.py
@@ -1,11 +1,17 @@
-import numpy as np
-
 from abc import ABC, abstractmethod
 
 from barcode_detection.core.bounding_box import BoundingBox
 
 
 class Localizer(ABC):
+    """
+       Class for Barcode Localization Algorithms
+       `get_boundings` method takes as a path to a directory that contains deblurred video frames as an input.
+       The output `List[List[BoundingBox]]` contains the coordinates of each detected barcode in each image
+       within the directory.
+
+    """
+
     @abstractmethod
-    def get_boundings(self, input_img: np.ndarray) -> list[BoundingBox]:
+    def get_boundings(self, input_dir: str) -> list[list[BoundingBox]]:
         pass
