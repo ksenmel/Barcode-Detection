e4d7be76ce04e415ea875e7c2aa69e70c4ee758f
ksenmel <ksemelashenko@gmail.com>
2024-09-20 23:46:16 +0300

    rewrite `get_boundings` for iyyun method

--- barcode_detection/localization/localize.py
+++ barcode_detection/localization/localize.py
@@ -1,17 +1,12 @@
 from abc import ABC, abstractmethod
 
-from barcode_detection.core.bounding_box import BoundingBox
-
 
 class Localizer(ABC):
     """
        Class for Barcode Localization Algorithms
        `get_boundings` method takes as a path to a directory that contains deblurred video frames as an input.
-       The output `List[List[BoundingBox]]` contains the coordinates of each detected barcode in each image
-       within the directory.
-
     """
 
     @abstractmethod
-    def get_boundings(self, input_dir: str) -> list[list[BoundingBox]]:
+    def get_boundings(self, input_dir: str):
         pass
