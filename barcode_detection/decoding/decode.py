594d85200ccc3c215862122327e6bf20d547149b
ksenmel <ksemelashenko@gmail.com>
2024-09-22 19:18:41 +0300

    chanage `decode`

--- barcode_detection/decoding/decode.py
+++ barcode_detection/decoding/decode.py
@@ -1,13 +1,9 @@
-import numpy as np
-
 from abc import ABC, abstractmethod
-
-from barcode_detection.core.bounding_box import BoundingBox
 
 
 class Decoder(ABC):
     @abstractmethod
     def decode(
-        self, input_img: np.ndarray, bounding_boxes: list[BoundingBox]
-    ) -> list[list[str] | None]:
+        self, images_dir: str, bounding_boxes_dir: str
+    ):
         pass
